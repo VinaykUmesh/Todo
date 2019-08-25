86542bbcb4d2e8c608d722a1fb155b801bffcb65
Umesh <luxurygeek.umesh@gmail.com>
2019-08-25 10:29:57 +0530

    Updates
    Added TodoslIst table and welcome to lisTodo redirection

--- todo-react/src/components/todo/TodoApp.jsx
+++ todo-react/src/components/todo/TodoApp.jsx
@@ -1,17 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter , Route ,Switch, Link} from 'react-router-dom';
+import './TodoApp.css';
 
 class TodoApp extends Component {
     render() {
         return (
             <div className="TodoApp">
-                <LoginComp/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" exact component={LoginComponent} />
+                        <Route path="/login" component={LoginComponent}/>
+                        <Route path="/welcome/:name" component={WelcomePage} />
+                        <Route path="/listTodo" component={ListTodo}/>
+                        <Route component={ErrorComponent} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
+
+class ListTodo extends Component{
+
+    state={
+        todos :[
+            {id : 1 , description : "Learn Something", done : false, targetDate : new Date()},
+            {id : 2 , description : "Do Something with that", done : false, targetDate : new Date()},
+            {id : 3 , description : "Get Onsite to someWhere", done : false, targetDate : new Date()},
+        ]
+    }
+
+    render(){
+        return <div className="loginmsg">
+            <h1>ListTodo</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Description</th>
+                        <th>Target Date</th>
+                        <th>Is Completed?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  
+                      {  this.state.todos.map(
+                          todos =>  <tr> 
+                          <td>{todos.id}</td>
+                          <td>{todos.description}</td>
+                          <td>{todos.done.toString()}</td>
+                          <td>{todos.targetDate.toString()}</td>
+                          </tr>
+                      )
+                        
+                      } 
+                </tbody>
+            </table>
+        </div>
+    }
+}
+
+class ErrorComponent extends Component {
+    render() {
+        return (
+            <div className="loginmsg">
+                404 Not Found
             </div>
         )
     }
 }
 
 
-class LoginComp extends Component{
+class LoginComponent extends Component{
 
     
         state = {
@@ -31,9 +91,7 @@ class LoginComp extends Component{
         event.preventDefault()
         console.log(this.state)
         if(this.state.username === "umesh" && this.state.password === "pass"){
-        console.log("successfull")
-        this.setState({showSuccessMsg:true,hasLoginFailed:false})
-        
+            this.props.history.push(`/welcome/${this.state.username}`);
         }
         else{
         this.setState({showSuccessMsg : false, hasLoginFailed:true})
@@ -41,9 +99,9 @@ class LoginComp extends Component{
     }
 
     render(){
-        return (<div>
-            <ShowCredentials hasLoginFailed={this.state.hasLoginFailed}/>
-            <ShowLoginSucess showSuccessMsg={this.state.showSuccessMsg}/>
+        return (<div className="TodoApp">
+            {this.state.showSuccessMsg && <div className="loginmsg">Login Successful</div>}
+            {this.state.hasLoginFailed && <div className="loginmsg">Invalid Credentials</div>}
             <form onSubmit={this.whenClicked}>
             UserName :<input type="text" name="username" value={this.state.username} onChange={this.inputHandler}/>
             Password :<input type="password" name="password" value={this.state.password} onChange={this.inputHandler}/>
@@ -54,24 +112,15 @@ class LoginComp extends Component{
 
 }
 
-function ShowCredentials(props){
-    if(props.hasLoginFailed){
-        return <div>Invalid Credentials</div>
-    }
-    else{
-        return null;
+class WelcomePage extends Component{
+    render(){
+        return <div className="TodoApp">
+            Welcome {this.props.match.params.name}. You can manage your todos by clicking <Link to="/listTodo">here</Link>
+        </div>
     }
 }
 
-function ShowLoginSucess(props){
-    if(props.showSuccessMsg){
-        return <div>
-            Login Sucessful
-        </div>
-    }else{
-        return null;
-    }
-}
+
 
 
 
--- todo-react/src/index.js
+++ todo-react/src/index.js
@@ -1,6 +1,7 @@
 import React ,{ Component }from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from './components/todo/TodoApp';
+import './index.css';
 
 class App extends Component {
     render() {
